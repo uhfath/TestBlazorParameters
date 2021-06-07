13476a196e329bc2ccec9a26ab03a68d7c14f42a
Kasbolat Kumakhov <uhfath@gmail.com>
2021-06-07 18:30:20 +0300

    Add project files.

---
+++ TestBlazorParameters/Pages/Index.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestBlazorParameters.Shared;
+
+namespace TestBlazorParameters.Pages
+{
+	public partial class Index : ComponentBase
+	{
+		private RenderFragment renderFragment;
+
+		private void OnTest()
+		{
+			StateHasChanged();
+		}
+
+		protected override void OnInitialized()
+		{
+			renderFragment = new RenderFragment(builder =>
+			{
+				var i = 0;
+				builder.OpenComponent(i++, typeof(SurveyPrompt));
+				builder.AddAttribute(i++, nameof(SurveyPrompt.Title), "test");
+				builder.AddAttribute(i++, nameof(SurveyPrompt.OnTest), (Action)OnTest);
+				builder.CloseComponent();
+			});
+		}
+	}
+}
